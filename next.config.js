9fd2f98e6b1b87307a5c8027f1a56dbafc3ba64b
Mubeen Ahmad <ahmedqazimubeen@gmail.com>
2026-01-28 03:02:29 +0500

    Convert next.config.ts to next.config.js for Next 14

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  // Remove reactCompiler: true,
+};
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  // Remove reactCompiler: true,
-};
-
-export default nextConfig;
